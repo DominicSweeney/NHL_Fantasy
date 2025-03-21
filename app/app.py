81e90116376d7e1b290a86082ef9d06640d60a14
ace-of-spade-s <79274455+ace-of-spade-s@users.noreply.github.com>
2025-03-21 14:54:03 +0000

    fixed databases implementaion and started working on client side api call

--- app/app.py
+++ app/app.py
@@ -3,11 +3,40 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 
+
 app = Flask(__name__)
+
+# Point to the same SQLite file you use in DataGrip
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Example Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    Password = db.Column(db.String(80), unique=True, nullable=False)
+    fav_team = db.Column(db.String(80), unique=True, nullable=False)
+
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
+create_tables()
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+#temp to check DELETE
+@app.route("/test")
+def test():
+    users=User.query.all()
+    for user in users:
+        print(user.username)
+    return render_template("test.html", users=users)
 
 @app.route("/home")
 def home():
@@ -34,21 +63,12 @@ def vs_computer():
 #     return jsonify(data)
 
 
-# Point to the same SQLite file you use in DataGrip
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///NHLfantasy.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
 
-# Example Model
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-
-@app.route('/')
-def index():
-    users = User.query.all()
-    return f"Users: {[user.username for user in users]}"
+# @app.route('/')
+# def index():
+#     users = User.query.all()
+#     return f"Users: {[user.username for user in users]}"
 
 if __name__ == '__main__':
     app.run(debug=True)
