d157890aad530d57a80399ea0ba0e617c12dfbc7
georgekav1 <149398975+georgekav1@users.noreply.github.com>
2025-03-25 22:27:37 +0000

    admin can delete users

--- app/app.py
+++ app/app.py
@@ -132,7 +132,7 @@ def register():
         db.session.commit()
 
         flash("Registration successful!", 'success')
-        return redirect(url_for('index'))
+        return redirect(url_for('login'))
     return render_template("Client/register.html")
 
 @app.route("/logout")
@@ -179,7 +179,7 @@ def admin_login():
 @app.route("/adminHome")
 @login_required
 def admin_home():
-    return render_template("Admin/adminHome.html")
+    return render_template("Admin/adminHome.html", username=current_user.username)
 
 @app.route("/admin_table")
 def admin_table():
@@ -208,26 +208,8 @@ def delete_user(user_id):
 
     db.session.delete(user)
     db.session.commit()
-    flash(f"User '{user.username}' has been deleted.", 'success')
+
     return redirect(url_for('manage_users'))
-
-@app.route("/edit_user/<int:user_id>", methods=['GET', 'POST'])
-@login_required
-def edit_user(user_id):
-    user = User.query.get_or_404(user_id)
-
-    if request.method == 'POST':
-        new_username = request.form['username']
-        new_email = request.form['email']
-
-        # Update user info
-        user.username = new_username
-        user.email = new_email
-        db.session.commit()
-        flash(f"User '{user.username}' has been updated.", 'success')
-        return redirect(url_for('manage_users'))
-
-    return render_template("editUser.html", user=user)
 
 # Main entry point for running the app
 if __name__ == '__main__':
