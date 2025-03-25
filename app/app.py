c476ba9556661c0373a5909532547e832c03d6c6
Thomexe <thomasmiedzybrodzki@gmail.com>
2025-03-25 21:44:01 +0000

    ass

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-import requests
+
 from flask import Flask, render_template, redirect, url_for, request, flash,abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
